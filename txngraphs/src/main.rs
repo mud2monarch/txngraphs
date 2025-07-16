cab92e7d8057b85e8a72e901c9cd1d8cc583ba34
Zach Wong <zach.wong@uniswap.org>
2025-07-16 16:01:47 -0400

    impl DataSource for DuneDexTradesDataSource is working :fire:

--- txngraphs/src/main.rs
+++ txngraphs/src/main.rs
@@ -1,16 +1,21 @@
 mod types;
-use polars::prelude::*;
-use types::*;
 use anyhow::Result;
+use polars::prelude::*;
 use std::sync::Arc;
+use types::*;
+use alloy_primitives::Address;
+use std::str::FromStr;
+use tracing_subscriber;
+use tracing::info;
 
 // Dune CSV example
 fn main() -> Result<()> {
-
+    tracing_subscriber::fmt::init();
     // Define schema overrides; can't use default i64 for token amounts
     let schema_changes = Schema::from_iter(vec![
         Field::new("token_sold_amount_raw".into(), DataType::String),
         Field::new("token_bought_amount_raw".into(), DataType::String),
+        Field::new("block_number".into(), DataType::UInt64),
     ]);
 
     // Load data from CSV
@@ -19,8 +24,17 @@ fn main() -> Result<()> {
         .with_schema_overwrite(Some(Arc::new(schema_changes)))
         .try_into_reader_with_file_path(Some("data/pi_token_trades_dune.csv".into()))?
         .finish()?;
-    
+
     // Create a new DuneDexTradesDataSource from the loaded data
     let data_source = DuneDexTradesDataSource::new(trades);
+
+    let transfers = data_source.get_transfers(
+        &Address::from_str("0xa23c6c374b372b6964ef7c1c00916e2b4f5a3629")?,
+        8610738,
+        8625670
+    )?;
+
+    info!("transfers.length: {}", transfers.len());
+
     Ok(())
 }
--- txngraphs/src/types.rs
+++ txngraphs/src/types.rs
@@ -1,13 +1,16 @@
-use petgraph::graphmap::DiGraphMap;
 use alloy_primitives::{
-    Address,
-    aliases::{
-        TxHash,
-        BlockNumber,
-        U256,
-    }
+    Address, B256,
+    aliases::{BlockNumber, TxHash, U256},
 };
+use anyhow::{Context, Result};
+use petgraph::graphmap::DiGraphMap;
 use polars::prelude::*;
+use std::{
+    fmt::Debug,
+    fmt::Display,
+};
+use std::str::FromStr;
+use tracing::info;
 
 ///
 /// TransferGraph
@@ -42,27 +45,60 @@ pub struct Transfer {
     pub amount: U256,
 }
 
+impl Transfer {
+    pub fn new(
+        tx_hash: TxHash,
+        block_number: BlockNumber,
+        from_address: Address,
+        to_address: Address,
+        token: Address,
+        amount: U256,
+    ) -> Self {
+        Self {
+            tx_hash,
+            block_number,
+            from_address,
+            to_address,
+            token,
+            amount,
+        }
+    }
+}
+
+impl Debug for Transfer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Transfer {{ tx_hash: {}, block_number: {}, from_address: {}, to_address: {}, token: {}, amount: {} }}", self.tx_hash, self.block_number, self.from_address, self.to_address, self.token, self.amount)
+    }
+}
+
+impl Display for Transfer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Transfer {{ tx_hash: {}, block_number: {}, from_address: {}, to_address: {}, token: {}, amount: {} }}", self.tx_hash, self.block_number, self.from_address, self.to_address, self.token, self.amount)
+    }
+}
+
 ///
 /// TransferDataSource
 ///
 /// A generic trait across different data sources.
 ///
 pub trait TransferDataSource {
-    fn get_transfers_from(
+    fn get_transfers(
         &self,
         address: &Address,
         block_start: BlockNumber,
         block_end: BlockNumber,
-    ) -> Vec<Transfer>;
+    ) -> anyhow::Result<Vec<Transfer>>;
 }
 
 ///
 /// DuneDexTradesDataSource
 ///
 /// An opinionated implementation of a data source based on Dune's dex.trades table.
-/// 
+///
 /// A DuneDexTradesDataSource wraps a polars DataFrame.
-/// 
+/// TODO: Implement Option<amount_usd>
+///
 /// Each row of the DataFrame should be a single DEX trade, and it should have the following columns:
 /// - `tx_from`
 /// - `tx_to`
@@ -73,7 +109,7 @@ pub trait TransferDataSource {
 /// - `token_bought_address`
 /// - `token_bought_amount_raw`
 /// - `amount_usd`
-/// 
+///
 /// For reference, see dune.com/queries/5488226.
 ///
 pub struct DuneDexTradesDataSource {
@@ -83,5 +119,72 @@ pub struct DuneDexTradesDataSource {
 impl DuneDexTradesDataSource {
     pub fn new(dex_trades: polars::prelude::DataFrame) -> Self {
         Self { dex_trades }
+    }
+}
+
+impl TransferDataSource for DuneDexTradesDataSource {
+    fn get_transfers(
+        &self,
+        address: &Address,
+        block_start: BlockNumber,
+        block_end: BlockNumber,
+    ) -> anyhow::Result<Vec<Transfer>> {
+        let addr_hex = format!("{address:#x}");
+
+        let filtered_trades = self
+            .dex_trades
+            .clone()
+            .lazy()
+            .filter(
+                col("tx_from")
+                    .eq(lit(addr_hex))
+                    .and(col("block_number").gt_eq(lit(block_start)))
+                    .and(col("block_number").lt_eq(lit(block_end))),
+            )
+            .collect()?;
+
+        info!("filtered_trades.height(): {}", filtered_trades.height());
+
+        let mut transfers = Vec::with_capacity(filtered_trades.height() as usize);
+
+        let col_tx_hash = filtered_trades.column("tx_hash")?.str()?;
+        let col_block_number = filtered_trades.column("block_number")?.u64()?;
+        let col_from_address = filtered_trades.column("tx_from")?.str()?;
+        let col_to_address = filtered_trades.column("tx_to")?.str()?;
+        let col_token = filtered_trades.column("token_sold_address")?.str()?;
+        let col_amount = filtered_trades.column("token_sold_amount_raw")?.str()?;
+
+        for row in 0..filtered_trades.height() {
+            let trade =
+                Transfer::new(
+                    B256::from_str(
+                        col_tx_hash
+                            .get(row)
+                            .with_context(|| format!("Failed to get tx_hash for {}", row))?,
+                    )?,
+                    col_block_number
+                        .get(row)
+                        .with_context(|| format!("Failed to get block_number for {}", row))?,
+                    Address::from_str(
+                        col_from_address
+                            .get(row)
+                            .with_context(|| format!("Failed to get tx_from for {}", row))?,
+                    )?,
+                    Address::from_str(
+                        col_to_address
+                            .get(row)
+                            .with_context(|| format!("Failed to get tx_to for {}", row))?,
+                    )?,
+                    Address::from_str(col_token.get(row).with_context(|| {
+                        format!("Failed to get token_sold_address for {}", row)
+                    })?)?,
+                    U256::from_str(col_amount.get(row).with_context(|| {
+                        format!("Failed to get token_sold_amount_raw for {}", row)
+                    })?)?,
+                );
+            transfers.push(trade);
+        }
+
+        Ok(transfers)
     }
 }
