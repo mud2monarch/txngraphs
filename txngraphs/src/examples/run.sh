82452f28f9c0c03a5ce0722313cedc7db1142bb5
mud2monarch <shepherd2sovereign@gmail.com>
2025-08-08 05:37:37 -0400

    working example

--- txngraphs/src/examples/run.sh
+++ txngraphs/src/examples/run.sh
@@ -1,6 +1,6 @@
-time ./txngraphs --root-address 0x3bc1588f8D987C9ED07B3796AA50ccBF10514326\
-    --token-address 0x622b6330f226bf08427dcad49c9ea9694604bf2d,0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2\
+./txngraphs --root-address 0x3bc1588f8D987C9ED07B3796AA50ccBF10514326\
+    --token-address 0x622b6330f226bf08427dcad49c9ea9694604bf2d\
     --block-start 20521409 \
-    --block-end 20543591 \
-    --max-depth 5 \
+    --block-end 20529409 \
+    --max-depth 3 \
     > result.txt
